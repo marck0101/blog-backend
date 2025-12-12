3c77271f4a2d42a8e56722f7877e86f2078fcceb
marck0101 <marcos.henrique.a7x@gmail.com>
2025-12-12 17:17:05 -0300

    ambiente de teste

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // url: "mongodb://0.0.0.0:27017/blog_project"
-  url: 'mongodb+srv://marckmhc_db_user:2adYUhbSxGrjER0G@cluster0.kbjm6ix.mongodb.net/?appName=Cluster0',
+  url: "mongodb://0.0.0.0:27017/blog_project"
+  // url: 'mongodb+srv://marckmhc_db_user:2adYUhbSxGrjER0G@cluster0.kbjm6ix.mongodb.net/?appName=Cluster0',
 }
 
 
