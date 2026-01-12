48be65cf8a6607f440b317e800488354998ae504
Marcos Henrique <marcos.henrique.a7x@gmail.com>
2026-01-12 15:06:28 -0300

    ajuste serverless api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 const serverless = require("serverless-http");
 const app = require("../app/app");
 
-module.exports = serverless(app);
+module.exports = serverless(app, {
+  callbackWaitsForEmptyEventLoop: false,
+});
